5f6a223e6fa654d438e62522629084cf4f195c51
Folky <thekiralog@ya.ru>
2023-06-26 20:44:33 +0400

    task done

--- main/Program.cs
+++ main/Program.cs
@@ -1,3 +1,15 @@
-﻿
-
+﻿string [] arr = new string[] {"fst", "second", ":-)"};
+List<string> list = new List<string>();
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length < 4){
+        list.Add(arr[i]);
+    }
+}
+string [] new_arr = new string[list.Count];
+for (int i = 0; i < list.Count; i++)
+{
+    new_arr[i] = list[i];
+}
+Console.Write(string.Join(", ", new_arr));
 
